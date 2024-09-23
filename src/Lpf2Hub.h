aa7785d6142da174001882ad4e792bbb28b09e37
rubenmuehlhans <85454114+rubenmuehlhans@users.noreply.github.com>
2024-09-23 20:38:13 +0200

    Update Lpf2Hub.h

--- src/Lpf2Hub.h
+++ src/Lpf2Hub.h
@@ -19,8 +19,8 @@
 using namespace std::placeholders;
 
 typedef void (*HubPropertyChangeCallback)(void *hub, HubPropertyReference hubProperty, uint8_t *pData);
-typedef void (*PortValueChangeCallback)(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData);
-//typedef void (*PortValueChangeCallback)(byte portNumber, DeviceType deviceType, uint8_t *pData, void *context);
+typedef void (*PortValueChangeCallback)(void *hub, byte portNumber, DeviceType deviceType, uint8_t *pData, void *context);
+//typedef void (*PortValueChangeCallbackWithContext)(byte portNumber, DeviceType deviceType, uint8_t *pData, void *context);
 
 struct Device
 {
